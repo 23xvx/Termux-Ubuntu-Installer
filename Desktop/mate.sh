aa09207872f0828ad52044230eb793afc0f1e722
23xvx <120558411+23xvx@users.noreply.github.com>
2023-05-24 02:14:04 +0800

    Update mate.sh

--- Desktop/mate.sh
+++ Desktop/mate.sh
@@ -7,9 +7,14 @@ echo "vncserver -kill :* ; rm -rf /tmp/.X1-lock ; rm -rf /tmp/.X11-unix/X1" >> /
 chmod +x /usr/local/bin/vncstart 
 chmod +x /usr/local/bin/vncstop 
 sleep 2
+clear
 echo "Please enter your vnc password"
 vncstart
-sleep 2 
+sleep 5
+vncstop 
+sleep 5 
+clear 
+vncstart
 dbus-launch dconf write /org/mate/desktop/interface/gtk-theme "'Yaru-MATE-dark'"
 sleep 2
 dbus-launch dconf write /org/mate/marco/general/theme "'Yaru-MATE-dark'"
