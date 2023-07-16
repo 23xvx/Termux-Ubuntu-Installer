2e37eda713eaf701688bfb7ad2caab294cabeddc
23xvx <120558411+23xvx@users.noreply.github.com>
2023-07-16 15:05:02 +0800

    Update firefox.sh

--- Apps/firefox.sh
+++ Apps/firefox.sh
@@ -3,7 +3,7 @@
 apt update 
 sudo apt install -y firefox
 rm -rf /etc/apt/preferences.d/fire*
-sudo apt remove firefox* -y
+sudo apt remove firefox -y
 sudo apt update && apt install firefox software-properties-common -y
 sudo add-apt-repository --yes ppa:mozillateam/ppa
 echo '
@@ -12,8 +12,8 @@ Pin: release o=LP-PPA-mozillateam
 Pin-Priority: 1001
 ' >> /etc/apt/preferences.d/mozilla-firefox
 echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";'>> /etc/apt/apt.conf.d/51unattended-upgrades-firefox
-apt update 
-apt remove firefox -y
-apt install firefox -y
+sudo apt update 
+sudo apt remove firefox -y
+sudo apt install firefox -y
 mv /etc/apt/trusted.gpg /etc/apt/trusted.gpg.d/ 
 rm -rf firefox.sh 
