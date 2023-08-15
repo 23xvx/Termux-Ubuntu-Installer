320a9af38ac1807ce18310b721ee4901f7f923c8
23xvx <120558411+23xvx@users.noreply.github.com>
2023-08-16 03:58:55 +0800

    Update firefox.sh

--- Apps/firefox.sh
+++ Apps/firefox.sh
@@ -14,5 +14,5 @@ Pin-Priority: 1001
 echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";'>> /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 sudo apt update 
 sudo apt remove firefox -y
-sudo apt install firefox -y
+sudo apt install firefox-esr -y
 rm -rf firefox.sh 
