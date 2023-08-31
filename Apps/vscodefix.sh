89d4549fa756a5c70e4f3d66f85181d4e50ad208
23xvx <noxic34@gmail.com>
2023-08-31 17:50:24 +0800

    Install vscode from official repository

--- Apps/vscodefix.sh
+++ Apps/vscodefix.sh
@@ -1,28 +1,13 @@
 #!/bin/sh 
-apt-get update 
-case `uname -m` in
-    aarch64)
-        archurl="arm64" ; 
-        wget "https://az764295.vo.msecnd.net/stable/704ed70d4fd1c6bd6342c436f1ede30d1cff4710/code_1.77.3-1681295476_${archurl}.deb" -P $HOME;;
-    arm*)
-        archurl="armhf" ;
-        wget "https://az764295.vo.msecnd.net/stable/704ed70d4fd1c6bd6342c436f1ede30d1cff4710/code_1.77.3-1681291917_${archurl}.deb" -P $HOME;;
-    x86)
-        archurl="amd64" 
-        wget "https://az764295.vo.msecnd.net/stable/704ed70d4fd1c6bd6342c436f1ede30d1cff4710/code_1.77.3-1681292746_${archurl}.deb" -P $HOME;;
-    x86_64)
-        archurl="amd64" ; 
-        wget "https://az764295.vo.msecnd.net/stable/704ed70d4fd1c6bd6342c436f1ede30d1cff4710/code_1.77.3-1681292746_${archurl}.deb" -P $HOME;;
-    *)
-        archurl="x" ;
-esac
-if [[ "$archurl" =~ ^([x])$ ]]; then 
-    echo "Unsupported architecture, cannot install VScode" 
-    sleep 1 
-else 
-    sudo apt install ./code* -y
-    sleep 1
-    rm ./*.deb 
-    sed -i 's/%F/--no-sandbox %F/g' /usr/share/applications/code.desktop 
-fi 
+# code from https://code.visualstudio.com/docs/setup/linux
+sudo apt-get update 
+sudo apt-get install wget gpg -y 
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https -y 
+sudo apt update
+sudo apt install code -y 
+sed -i 's/%F/--no-sandbox %F/g' /usr/share/applications/code.desktop 
 rm -rf $HOME/vscodefix.sh 
