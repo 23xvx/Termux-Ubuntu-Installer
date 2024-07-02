3b8d91dcba75b5bc1f7160d7cd18287a223dbba8
23xvx <120558411+23xvx@users.noreply.github.com>
2024-07-03 04:08:17 +0800

    Update firefox.sh

--- Apps/firefox.sh
+++ Apps/firefox.sh
@@ -1,6 +1,7 @@
 #!/bin/sh 
 # source by udroid 
-sudo apt update 
+sudo apt update
+sudo apt install gpg  software-properties-common -y
 sudo add-apt-repository --yes ppa:mozillateam/ppa
 echo '
 Package: *
